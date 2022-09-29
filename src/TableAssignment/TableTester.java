74cb316799ecf6963a6eab46d838646a8986c202
Jonah-B-Kehillah <jbarnett2023@kehillahstudent.org>
2022-09-29 09:27:19 -0700

    Added test file framework

--- src/TableAssignment/TableTester.java
+++ src/TableAssignment/TableTester.java
@@ -1,4 +1,12 @@
 package src.TableAssignment;
 
 public class TableTester {
+	
+	private static int passed = 0;
+	private static int failed = 0;
+	
+	public static void main(String[] args) {
+	
+	}
+	
 }
