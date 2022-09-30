395e9120920a3629b935a6fe7a4a054c27b1b947
Jonah-B-Kehillah <jbarnett2023@kehillahstudent.org>
2022-09-29 20:39:15 -0700

    Implemented equals() for Data class

--- src/TableAssignment/Data.java
+++ src/TableAssignment/Data.java
@@ -1,5 +1,7 @@
 package src.TableAssignment;
 
+import java.lang.reflect.Type;
+
 public class Data<T> {
     private T value;
     private final int key;
@@ -11,5 +13,16 @@ public class Data<T> {
     public void setValue(T _value) {this.value = _value;}
     public T getValue() {return this.value;}
     public int getKey() {return this.key;}
-
+    
+    @Override
+    public boolean equals(Object val) {
+        if (value.getClass() == val.getClass()) {
+            return value.equals(val);
+        } else if (val.getClass() == this.getClass()) {
+            return this.value == ((Data<?>) val).getValue() && this.key == ((Data<?>) val).getKey();
+        } else {
+            return this.equals(val);
+        }
+    }
+    
 }
