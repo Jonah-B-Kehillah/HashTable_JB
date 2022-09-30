d26cb7ae7619638bbd8890bae4bf82d9b2bcbc16
Jonah-B-Kehillah <jbarnett2023@kehillahstudent.org>
2022-09-29 19:44:22 -0700

    Updated testing framework

--- src/TableAssignment/TableTester.java
+++ src/TableAssignment/TableTester.java
@@ -2,11 +2,24 @@ package src.TableAssignment;
 
 public class TableTester {
 	
+	private static int tests = 0;
 	private static int passed = 0;
 	private static int failed = 0;
 	
 	public static void main(String[] args) {
 	
+	}
+	
+	
+	
+	private static void test(boolean output, String method) {
+		tests++;
+		System.out.println(String.format(
+			"Test #%d %s (%s)",
+			tests,
+			(output ? "passed" : "failed"),
+			method
+		));
 	}
 	
 }
